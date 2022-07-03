c4f8c0ffa9ddc2c1b733bc7f89967845ffaf6c28
Suriya Murugesh <suriyamu@student.tce.edu>
2022-07-03 19:22:02 +0530

    You can proceed further only if ckt design is correct
    * Added a check condition with a boolean variable (design_verified) , so that if the bool value is true, the user can go to the next image.. else he can't.
    
    * Added hover effects for next, prev, reset buttons
    
    * Replaced pos_unbias_page ckts and graph images with larger images

--- js_files_for_simulation_pages/1_pos_unbias.js
+++ js_files_for_simulation_pages/1_pos_unbias.js
@@ -1,5 +1,6 @@
 // global variables
-var vpeak_value = 0
+var vpeak_value = 0;
+var design_verified = false;
 
 
 
@@ -7,7 +8,7 @@ function incrementStep() {
     
     let count = document.getElementById("count").innerText;
 
-    if(count<6) {
+    if(count<6 && design_verified == true) {
         document.getElementById("count").innerText++;
     }
     change_Circuit_Graph_Explanation();
@@ -33,36 +34,42 @@ function change_Circuit_Graph_Explanation() {
     const count = document.getElementById("count").innerText;
 
     //changing circuit diagram
-    document.getElementById("diagram").innerHTML = '<img src="images/Pos_unbiased_clamper/Circuit_pics/Slide'+count+'.JPG" alt="">';
+    document.getElementById("diagram").innerHTML = '<img src="images/1_Pos_unbiased_clamper/Circuit_pics/Slide'+count+'.JPG" alt="">';
 
     //changing graph_image
-    document.getElementById("output_graph").src="images/Pos_unbiased_clamper/Graph_pics/Slide"+count+".JPG";
+    document.getElementById("output_graph").src="images/1_Pos_unbiased_clamper/Graph_pics/Slide"+count+".JPG";
 
 
     //changing explanation "text"
-    if(count==1) {
-        document.getElementById("explanation").innerHTML = '<h2>Click "Next" button to begin! </h2>';
+    if(design_verified==false) {
+        document.getElementById("explanation").innerHTML = '<h2>Choose the components correctly.<br>Verify your design<br>Then press the "Next" button.</h2>';
     }
-
-    if(count==2) {
-        document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">During the First Positive Half Cycle, </span>the diode is reverse biased and so acts as an open circuit. The current flows from supply to the load via capacitor. Since time constant is more than 10 times time period,  capacitor does not even start to charge and output voltage is almost equal to input voltage.</h2>';
+    else {
+        if(count==1) {
+            document.getElementById("explanation").innerHTML = '<h2>Click "Next" button to begin! </h2>';
+        }
+    
+        if(count==2) {
+            document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">During the First Positive Half Cycle, </span>the diode is reverse biased and so acts as an open circuit. The current flows from supply to the load via capacitor. Since time constant is more than 10 times time period,  capacitor does not even start to charge and output voltage is almost equal to input voltage.</h2>';
+        }
+    
+        if(count==3) {
+            document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">During the first negative half cycle, </span>the diode is forward biased, hence acts as a short circuit. Now the output voltage is equal to zero. Due to the short circuit, large current flows and hence charging the capacitor to peak value.</h2>';
+        }
+    
+        if(count==4) {
+            document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">When first negative peak is reached, </span>the voltage across the diode is zero. This turns off the diode. Now the output voltage is the sum of input voltage and capacitor voltage. The capacitor now acts like a DC source, thus shifting the waveform above by a DC value of +peak voltage.</h2>';
+        }
+    
+        if(count==5) {
+            document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">After first negative peak, </span>the voltage across the diode will always be lesser than or equal to zero. Hence, the diode will be reverse biased always hereafter. Hence output voltage will be the sum of input AC voltage and capacitor voltage.</h2>';
+        }
+    
+        if(count==6) {
+            document.getElementById("explanation").innerHTML = '<h2>After first negative peak is reached, the output voltage will be in <span class="bolder_black">steady state condition.</span> The capacitor will also maintain the same votlage as it is continuously connected to supply and has no other path to discharge.</h2>';
+        }
     }
-
-    if(count==3) {
-        document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">During the first negative half cycle, </span>the diode is forward biased, hence acts as a short circuit. Now the output voltage is equal to zero. Due to the short circuit, large current flows and hence charging the capacitor to peak value.</h2>';
-    }
-
-    if(count==4) {
-        document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">When first negative peak is reached, </span>the voltage across the diode is zero. This turns off the diode. Now the output voltage is the sum of input voltage and capacitor votlage. The capacitor now acts like a DC source, thus shifting the waveform above by a DC value of +peak voltage.</h2>';
-    }
-
-    if(count==5) {
-        document.getElementById("explanation").innerHTML = '<h2><span class="bolder_black">After first negative peak, </span>the voltage across the diode will always be lesser than or equal to zero. Hence, the diode will be reverse biased always hereafter. Hence output voltage will be the sum of input AC voltage and capacitor voltage.</h2>';
-    }
-
-    if(count==6) {
-        document.getElementById("explanation").innerHTML = '<h2>After first negative peak is reached, the output voltage will be in <span class="bolder_black">steady state condition.</span> The capacitor will also maintain the same votlage as it is continuously connected to supply and has no other path to discharge.</h2>';
-    }
+    
         
 }
 
@@ -105,17 +112,20 @@ function validate()
     // Edge case - 1
     if(input_v_value == "") {
         document.getElementById("check_design").innerHTML = '<h5><span class="error">Enter the value of Input voltage (RMS value).</span></h5>'; 
+        design_verified = false;
         return;
     }
 
     // Edge case - 2
     if(input_v_value < 0) {
         document.getElementById("check_design").innerHTML = '<h5 class="error">RMS value of AC signal cannot be negative.</h5>';
+        design_verified = false;
         return;
     }
 
     if(input_v_value == 0) {
         document.getElementById("check_design").innerHTML = '<h5 class="error">Input voltage cannot be zero.</h5>';
+        design_verified = false;
         return;
     }
 
@@ -127,6 +137,7 @@ function validate()
     if(input_v_value > 353.55) {
         // (1000/2)/root 2 = 353.55
         document.getElementById("check_design").innerHTML = '<h5><span class="error">The diodes available cannot withstand this voltage.</span></h5><br><h5>Reason: V<sub>p</sub>=V<sub>rms</sub>*&radic;2 = '+ input_v_value + '*&radic;2 = ' + vpeak_value + 'V<br>The maximum peak output voltage = 2 x V<sub>p</sub>= '+vpeak_value*2 +'V (in positive unbiased clamper).<br>This voltage will be applied across the reverse biased diode. <br>The PIV of all diodes are below 1000V.<br><span class="error">Hence this voltage cannot be chosen</span></h5>';
+        design_verified = false;
         return;
     }
 
@@ -141,6 +152,7 @@ function validate()
             }
             else {
                 document.getElementById("check_design").innerHTML = '<h5 class="error">The diode ' + diode_name + ' can not withstand this voltage.</h5><br><h5>Reason: V<sub>p</sub>=V<sub>rms</sub>*&radic;2 = '+ input_v_value + '*&radic;2 = ' + vpeak_value + 'V<br>The maximum peak output voltage = 2 x V<sub>p</sub>= '+vpeak_value*2 +'V (in positive unbiased clamper).<br>This voltage will be applied across the reverse biased diode. <br>The PIV of this diode is '+ getPIV(diode_name)+'V.<br><span class="error">Hence this diode cannot be chosen</span></h5>';
+                design_verified = false;
                 return;
             }
         }
@@ -151,6 +163,7 @@ function validate()
             }
             else {
                 document.getElementById("check_design").innerHTML = '<h5 class="error">The diode ' + diode_name + ' can not withstand this voltage.</h5><br><h5>Reason: V<sub>p</sub>=V<sub>rms</sub>*&radic;2 = '+ input_v_value + '*&radic;2 = ' + vpeak_value + 'V<br>The maximum peak output voltage = 2 x V<sub>p</sub>= '+vpeak_value*2 +'V (in positive unbiased clamper).<br>This voltage will be applied across the reverse biased diode. <br>The PIV of this diode is '+ getPIV(diode_name)+'V.<br><span class="error">Hence this diode cannot be chosen</span></h5>';
+                design_verified = false;
                 return;
             }
         }
@@ -162,6 +175,7 @@ function validate()
             }
             else {
                 document.getElementById("check_design").innerHTML = '<h5 class="error">The diode ' + diode_name + ' can not withstand this voltage.</h5><br><h5>Reason: V<sub>p</sub>=V<sub>rms</sub>*&radic;2 = '+ input_v_value + '*&radic;2 = ' + vpeak_value + 'V<br>The maximum peak output voltage = 2 x V<sub>p</sub>= '+vpeak_value*2 +'V (in positive unbiased clamper).<br>This voltage will be applied across the reverse biased diode. <br>The PIV of this diode is '+ getPIV(diode_name)+'V.<br><span class="error">Hence this diode cannot be chosen</span></h5>';
+                design_verified = false;
                 return;
             }
         }
@@ -178,6 +192,7 @@ function validate()
         }
         else {
             document.getElementById("check_design").innerHTML += '<h5>&#964; = R*C = '+ (res_val*getCapVal(capacitance))/1000 +'ms. Since &#964; is not greater than 10 times time period (20 ms). <span class="error">R and C are not chosen correctly</span></h5>';
+            design_verified = false;
             return;
         }
     }
@@ -188,6 +203,8 @@ function validate()
         }
     }
 
+    
+    design_verified = true;
     //this code will e executed if all the design are correct.
     document.getElementById("check_design").innerHTML += '<h5 class="acknowledgement">Your circuit design is correct.</h5>';
     //changing value of Vp below waveform
